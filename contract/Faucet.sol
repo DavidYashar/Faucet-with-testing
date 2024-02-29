fa669a95b05112156e2b06cd5c29515c5c0b402e
Yashar Baradarvar <83546651+DavidYashar@users.noreply.github.com>
2024-02-29 06:25:38 +0300

    Update Faucet.sol

--- contract/Faucet.sol
+++ contract/Faucet.sol
@@ -1,1 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
 
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Faucet {
+    address payable public owner;
+
+    constructor() payable {
+        owner = payable(msg.sender);
+
+    }
+
+    function withdraw(uint _amount) payable public {
+        //user can only withdraw -1 ETH at a time,
+
+        require(_amount <=  100000000000000000 );
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success, "transfer failed");
+    }
+
+    function withdrawAll() onlyOwner public {
+        //only owner can withdraw all the faucet 
+        (bool sent, ) = owner.call{value: address(this).balance}("");
+        require(sent, "failed to sent all ether");
+
+    }
+
+    function destroyFaucet() onlyOwner public {
+        owner.transfer(address(this).balance);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner can withdraw all the ether");
+        _;
+    }
+}
